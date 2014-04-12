4e1411c283e87908cf27552c184e9d865b909328
Evan Tahler <evan@taskrabbit.com>
2014-04-12 15:05:52 -0700

    db name typo

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,7 +1,7 @@
 exports.default = { 
   sequelize: function(api){
     return {
-      "database"    : "DEVELOPMENT_DB",
+      "database"    : "DEVELOPMNET_DB",
       "dialect"     : "mysql",
       "port"        : 3306,
       "host"        : "127.0.0.1", 
