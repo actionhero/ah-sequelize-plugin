2fa405466db900ad7dc948af0b4cf61d59231679
Victor Heng <bakavic@gmail.com>
2015-07-13 15:15:27 +0800

    don't throw an error if config dir isn't there - just silently create it.

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -4,6 +4,7 @@ var fs     = require('fs');
 var path   = require('path');
 var mkdirp = require('mkdirp');
 
+var projectConfigDir = path.normalize(process.cwd() + '/../../config/');
 var localConfigFile   = path.normalize(__dirname + '/../config/sequelize.js');
 var projectConfigFile = path.normalize(process.cwd() + '/../../config/sequelize.js');
 
@@ -15,13 +16,14 @@ try {
 } catch (ex) {
   //unable to stat file because it doesn't exist
   console.log("copying " + localConfigFile + " to " + projectConfigFile);
+  mkdirp.sync(path.normalize(projectConfigDir));
   fs.createReadStream(localConfigFile).pipe(fs.createWriteStream(projectConfigFile));
 
   // Only try to copy the files required for cli operations if sequelize.js is being newly created.
   try {
        fs.lstatSync(projectRcFile);
   } catch (ex) {
-      console.log("copying " + localRcFile + " to " + projectRcFile + " for ");
+      console.log("copying " + localRcFile + " to " + projectRcFile);
       fs.createReadStream(localRcFile).pipe(fs.createWriteStream(projectRcFile));
   }
 }
