e3a32af4d18873ec83a0e03263904a3b0323f3f2
Muhamed Krlić <krlicmuhamed@gmail.com>
2016-06-22 14:48:20 +0200

    Priority aligned with logger. Added winston/api.logger.info logging.

--- initializers/sequelize.js
+++ initializers/sequelize.js
@@ -4,13 +4,13 @@ var Sequelize         = require('sequelize');
 var Umzug             = require('umzug');
 
 module.exports = {
-  loadPriority: 101, // aligned with actionhero's redis initializer
-  startPriority: 101, // aligned with actionhero's redis initializer
-  stopPriority: 101, // aligned with actionhero's redis initializer
+  loadPriority: 121, // aligned with actionhero's redis and logger initializer
+  startPriority: 121, // aligned with actionhero's redis and logger initializer
+  stopPriority: 121, // aligned with actionhero's redis and logger initializer
 
   initialize: function(api, next){
     api.models = {};
-
+    api.config.sequelize.logging = api.logger.info;
     var sequelizeInstance = new Sequelize(
       api.config.sequelize.database,
       api.config.sequelize.username,
@@ -87,7 +87,7 @@ module.exports = {
       },
 
       autoMigrate: function(next) {
-        if(api.config.sequelize.autoMigrate === null || 
+        if(api.config.sequelize.autoMigrate === null ||
           api.config.sequelize.autoMigrate === undefined ||
           api.config.sequelize.autoMigrate) {
           checkMetaOldSchema(api, umzug).then(function() {
