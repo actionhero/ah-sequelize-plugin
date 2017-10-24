9eb1b55c940033e86acabec851b0a4aa2818263b
Zach Cardoza <2280384+bayssmekanique@users.noreply.github.com>
2017-10-24 10:08:57 -0700

    Adds config to postinstall

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -4,6 +4,7 @@ const fs = require('fs')
 const path = require('path')
 const mkdirp = require('mkdirp')
 
+const localConfigFile = path.normalize(path.join(__dirname, '/../config/sequelize.js'))
 const projectConfigFile = path.normalize(process.cwd() + '/../../config/sequelize.js')
 
 const localRcFile = path.normalize(path.join(__dirname, '/../config/.sequelizerc'))
@@ -12,6 +13,8 @@ const projectRcFile = path.normalize(process.cwd() + '/../../.sequelizerc')
 try {
   fs.lstatSync(projectConfigFile)
 } catch (ex) {
+  console.log('copying ' + localConfigFile + ' to ' + projectConfigFile)
+  fs.createReadStream(localConfigFile).pipe(fs.createWriteStream(projectConfigFile))
   // Only try to copy the files required for cli operations if sequelize.js is being newly created.
   try {
     fs.lstatSync(projectRcFile)
@@ -25,6 +28,6 @@ try {
   mkdirp.sync(path.normalize(process.cwd() + '/../../' + f))
 })
 
-console.warn('Notice:')
-console.warn('npm run actionhero link -- --name ah-sequelize-plugin')
-console.warn('To read more about this, https://docs.actionherojs.com/tutorial-plugins.html')
+// console.warn('Notice:')
+// console.warn('npm run actionhero link -- --name ah-sequelize-plugin')
+// console.warn('To read more about this, https://docs.actionherojs.com/tutorial-plugins.html')
