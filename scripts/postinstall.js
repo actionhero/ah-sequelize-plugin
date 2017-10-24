f4b8310ff8fc27e140ab555781e2e7795a262451
Zach Cardoza <2280384+bayssmekanique@users.noreply.github.com>
2017-10-24 11:13:52 -0700

    Fixes documentation and install scripts (#48)
    Fixes postinstall script to copy config file.
    Prevents postinstall script from running during development.
    Fixes associations.js script to properly iterate model object.
    Removes all references to --link command following install.
    Updates readme to proper plugin configuration.
    
    Resolves #47

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -4,27 +4,36 @@ const fs = require('fs')
 const path = require('path')
 const mkdirp = require('mkdirp')
 
+const projectApiConfigLocation = path.normalize(process.cwd() + '/../../config/api.js')
+
+const localConfigFile = path.normalize(path.join(__dirname, '/../config/sequelize.js'))
 const projectConfigFile = path.normalize(process.cwd() + '/../../config/sequelize.js')
 
 const localRcFile = path.normalize(path.join(__dirname, '/../config/.sequelizerc'))
 const projectRcFile = path.normalize(process.cwd() + '/../../.sequelizerc')
 
 try {
-  fs.lstatSync(projectConfigFile)
-} catch (ex) {
-  // Only try to copy the files required for cli operations if sequelize.js is being newly created.
+  fs.lstatSync(projectApiConfigLocation)
+  // Only run if api config file exists (prevents install whlie in development)
   try {
-    fs.lstatSync(projectRcFile)
+    fs.lstatSync(projectConfigFile)
   } catch (ex) {
-    console.log('copying ' + localRcFile + ' to ' + projectRcFile)
-    fs.createReadStream(localRcFile).pipe(fs.createWriteStream(projectRcFile))
+    // Only try to copy the files required for cli operations if sequelize.js is being newly created.
+    console.log('copying ' + localConfigFile + ' to ' + projectConfigFile)
+    fs.createReadStream(localConfigFile).pipe(fs.createWriteStream(projectConfigFile))
+    try {
+      fs.lstatSync(projectRcFile)
+    } catch (ex) {
+      console.log('copying ' + localRcFile + ' to ' + projectRcFile)
+      fs.createReadStream(localRcFile).pipe(fs.createWriteStream(projectRcFile))
+    }
   }
+
+  ['models', 'test/fixtures'].forEach(function (f) {
+    let t = path.normalize(process.cwd() + '/../../' + f)
+    console.log('creating ' + t)
+    mkdirp.sync(t)
+  })
+} catch (ex) {
+  console.log('postinstall script skipped')
 }
-
-['models', 'test/fixtures'].forEach(function (f) {
-  mkdirp.sync(path.normalize(process.cwd() + '/../../' + f))
-})
-
-console.warn('Notice:')
-console.warn('npm run actionhero link -- --name ah-sequelize-plugin')
-console.warn('To read more about this, https://docs.actionherojs.com/tutorial-plugins.html')
